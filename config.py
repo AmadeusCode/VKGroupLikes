e0f0a75d02a424efbbf6254f70b446ad2ed75b4c
Neuralink <47272084+AmadeusCode@users.noreply.github.com>
2019-04-18 22:19:06 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,9 +4,9 @@ config = {
     'password'          :   'YOUR_PASSWORD',    # Your VK password
     'token'             :   'YOUR_TOKEN',       # Your VK token
     'group_id'          :    -1,		# Group id Like -xxxxxxxxx
-    'count_last_posts'  :    1,	                # Count of last posts to be liked
+    'count_last_posts'  :    10,	                # Count of last posts to be liked
     'liked_last_posts'  :    True,			    # Procedure of Liked Last Count of Wall Posts. | True or False
     'liked_new_posts'   :    True,		        # Start deamon to liked new group post.        | True or False
     'cooldown_like'     :    1,                 # Cooldown between liked last group posts in seconds. !Very low cooldawn may cause captcha error
-    'cooldown_check'    :    360                # Cooldown between check new posts from group in seconds.
+    'cooldown_check'    :    300                # Cooldown between check new posts from group in seconds.
 }
