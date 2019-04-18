c83fc953f61350e044fce8d0a4ef07765df8bbea
Neuralink <47272084+AmadeusCode@users.noreply.github.com>
2019-04-18 22:03:29 +0300

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ config = {
     'group_id'          :    -1,		# Group id Like -xxxxxxxxx
     'count_last_posts'  :    1,	                # Count of last posts to be liked
     'liked_last_posts'  :    True,			    # Procedure of Liked Last Count of Wall Posts. | True or False
-    'liked_new_posts'   :    False,		        # Start deamon to liked new group post.        | True or False
+    'liked_new_posts'   :    True,		        # Start deamon to liked new group post.        | True or False
     'cooldown_like'     :    1,                 # Cooldown between liked last group posts in seconds. !Very low cooldawn may cause captcha error
     'cooldown_check'    :    360                # Cooldown between check new posts from group in seconds.
 }
